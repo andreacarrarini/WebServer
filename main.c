4efc1d82f1c4c3b3d348ea8d86484e76ee23c7fe
giuseppe <jph94bambu@hotmail.it>
2016-11-17 14:08:41 +0100

    abc

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
-    printf("hello world!\nAndrea lavati il culo");
+
+    char *a1 = "/home/giuseppe/Scaricati";
+    char *a2 = "123.png";
+    char *a3 = a1;
+    char *a4 = "1234.png";
+
+    int q = 50;
+
+    convert_img(a1,a2,a3,a4,q);
+
+    return 0;
+}
+
+/*
+ * ------------------CONVERT_IMG-------------------------------
+ *
+ * crea una nuova immagine ridimensionata.
+ * prende in input il percorso dell'immagine da ridimensionare,
+ * il percordo di destinazione e il fattore di qualità;
+ * viene creata una stringa da passare come comando al terminale;
+ * questo comando fa uso della funzione convert presente nella
+ * libreria imagemagick.
+ */
+
+
+int convert_img(char *path, char *name, char *f_path, char *f_name, int q){
+
+    // comand format: "convert path/name -resize q% s_path/s_name;exit"
+
+    char *final_comand = (char *) malloc(30 + strlen(path) + strlen(name) + strlen(f_path) + strlen(f_name));
+    char *percentage = "%";
+
+    sprintf(final_comand, "convert %s/%s -resize %d%s %s/%s;exit", path, name, q, percentage, f_path, f_name);
+    system(final_comand);
+    free(final_comand);
     return 0;
 }
