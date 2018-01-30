b96e4596acfbafe4796f79defdd22a0038f6012a
andreacarrarini <drughi4ever94@gmail.com>
2018-01-30 11:14:35 +0100

    pulizia codice

--- functions.h
+++ functions.h
@@ -56,13 +56,13 @@ void error_found(char *s);
 
 char *search_file(struct image_struct *image, char *name_cached_img, char *img_to_send, struct cache_struct *cache_ptr, char *char_time, char *http_response);
 
-int free_cache_slot(struct image_struct *i, char *char_time, char *http_response);
+int free_cache_slot(struct image_struct *image, char *char_time, char *HTTP_response);
 
-int delete_image(char *img_to_send, char *char_time, char *http_response);
+int delete_image(char *image_to_send, char *char_time, char *HTTP_response);
 
-int insert_in_cache(char *path, int quality_factor, char *name_cached_img, struct image_struct *i, char *char_time, char *http_response);
+int insert_in_cache(char *path, int quality_factor, char *name_cached_img, struct image_struct *image, char *char_time, char *HTTP_repsonse);
 
-void look_for_cached_img(int CACHE, char *name_cached_image);
+void look_for_cached_img(int CACHE_COUNTER, char *name_cached_image);
 
 void write_on_stream(char *string, FILE *file);
 
@@ -80,13 +80,13 @@ void error_found(char *s);
 
 void catch_signal(void);
 
-void lock(pthread_mutex_t *m);
+void lock(pthread_mutex_t *mutex);
 
-void unlock(pthread_mutex_t *m);
+void unlock(pthread_mutex_t *mutex);
 
-void wait_t(pthread_cond_t *c, pthread_mutex_t *m);
+void wait_t(pthread_cond_t *cond, pthread_mutex_t *mutex);
 
-void signal_t(pthread_cond_t *c);
+void signal_t(pthread_cond_t *cond);
 
 FILE *open_LOG_file(const char *path_to_LOG_dir);
 
@@ -102,7 +102,7 @@ ssize_t send_HTTP_message(int socket_fd, char *s, ssize_t msg_dim);
 
 char *get_image(char *image_name, size_t image_dim, char *directory);
 
-void free_time_http(char *time, char *http);
+void free_time_HTTP_response(char *time, char *HTTP_response);
 
 void split_HTTP_message(char *HTTP_request_buffer, char **line_request);
 
@@ -118,7 +118,7 @@ void check_and_build(char *s, char **html, size_t *dim);
 
 void check_WebServer_images(int perc);
 
-void kill_th(struct threads_sync_struct *k);
+void kill_thread(struct threads_sync_struct *threads_sync_struct);
 
 /*
  * commento di prova
--- thread_functions.c
+++ thread_functions.c
@@ -8,90 +8,90 @@
 // Used to block SIGPIPE sent from send function
 void catch_signal(void) {
 
-    struct sigaction sa;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_handler = SIG_IGN;
-    if (sigaction(SIGPIPE, &sa, NULL) == -1)
-        error_found("Error in sigaction\n");
+    struct sigaction struct_sigaction;
+    sigemptyset(&struct_sigaction.sa_mask);
+    struct_sigaction.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &struct_sigaction, NULL) == -1)
+        error_found("catch_signal: Error in sigaction\n");
 }
 
 // Used to get mutex to access a memory
 //  area shared by multiple execution flows
-void lock(pthread_mutex_t *m) {
+void lock(pthread_mutex_t *mutex) {
 
-    if (pthread_mutex_lock(m) != 0)
-        error_found("Error in pthread_mutex_lock\n");
+    if (pthread_mutex_lock(mutex) != 0)
+        error_found("lock: Error in pthread_mutex_lock\n");
 }
 
 // Used to release mutex
-void unlock(pthread_mutex_t *m) {
+void unlock(pthread_mutex_t *mutex) {
 
-    if (pthread_mutex_unlock(m) != 0)
-        error_found("Error in pthread_mutex_unlock\n");
+    if (pthread_mutex_unlock(mutex) != 0)
+        error_found("unlock: Error in pthread_mutex_unlock\n");
 }
 
 // Used waiting for the occurrence of an event
-void wait_t(pthread_cond_t *c, pthread_mutex_t *m) {
+void wait_t(pthread_cond_t *cond, pthread_mutex_t *mutex) {
 
-    if (pthread_cond_wait(c, m) != 0)
-        error_found("Error in pthread_cond_wait\n");
+    if (pthread_cond_wait(cond, mutex) != 0)
+        error_found("wait_t: Error in pthread_cond_wait\n");
 }
 
 // Used to send a signal to a thread
-void signal_t(pthread_cond_t *c) {
+void signal_t(pthread_cond_t *cond) {
 
-    if (pthread_cond_signal(c) != 0)
-        error_found("Error in pthread_cond_signal\n");
+    if (pthread_cond_signal(cond) != 0)
+        error_found("signal_t: Error in pthread_cond_signal\n");
 }
 
-// Used by kill_th function
-void for_kill(int n_th, struct threads_sync_struct *k) {
+// Used by kill_thread function
+void mark_to_kill(int threads_number, struct threads_sync_struct *threads_sync_struct) {
 
     int i, j;
 
-    for (i = j = 0; i < n_th && j < MAX_CONNECTION; ++j)
-        if (k -> client_socket_list[j] == -1) {
-            k -> client_socket_list[j] = -2;
-            signal_t(k -> threads_cond_list + j);
+    for (i = j = 0; i < threads_number && j < MAX_CONNECTION; ++j)
+        if (threads_sync_struct -> client_socket_list[j] == -1) {
+            threads_sync_struct -> client_socket_list[j] = -2;
+            signal_t(threads_sync_struct -> threads_cond_list + j);
             ++i;
         }
 
     // If there are not enough threads ready (with -1 flag)
-    if (i < n_th)
-        k -> threads_to_kill = n_th - i;
+    if (i < threads_number)
+        threads_sync_struct -> threads_to_kill = threads_number - i;
 }
 
 // Used to kill threads
-void kill_th(struct threads_sync_struct *k) {
+void kill_thread(struct threads_sync_struct *threads_sync_struct) {
 
-    int n_th = 0;
+    int threads_number = 0;
 
-    if (k -> min_active_threads_treshold > MIN_THREAD_TRESHOLD) {
-        int old_thr = (k -> min_active_threads_treshold - MIN_THREAD_TRESHOLD / 2) * 2 / 3;
+    if (threads_sync_struct -> min_active_threads_treshold > MIN_THREAD_TRESHOLD) {
+        int old_thr = (threads_sync_struct -> min_active_threads_treshold - MIN_THREAD_TRESHOLD / 2) * 2 / 3;
         // To bring system to the default thread count
-        if (!k -> connections) {
-            k -> min_active_threads_treshold = MIN_THREAD_TRESHOLD;
-            n_th = k -> active_threads - k -> min_active_threads_treshold;
-            k -> threads_to_kill = 0;
-            for_kill(n_th, k);
+        if (!threads_sync_struct -> connections) {
+            threads_sync_struct -> min_active_threads_treshold = MIN_THREAD_TRESHOLD;
+            threads_number = threads_sync_struct -> active_threads - threads_sync_struct -> min_active_threads_treshold;
+            threads_sync_struct -> threads_to_kill = 0;
+            mark_to_kill(threads_number, threads_sync_struct);
             return;
-        } else if (k -> connections < old_thr) {
+        } else if (threads_sync_struct -> connections < old_thr) {
             // Gradual deallocation
-            if (k -> min_active_threads_treshold == MAX_CONNECTION) {
-                if (!(n_th = (MAX_CONNECTION - MIN_THREAD_TRESHOLD) % (MIN_THREAD_TRESHOLD / 2)))
-                    n_th = MIN_THREAD_TRESHOLD / 2;
+            if (threads_sync_struct -> min_active_threads_treshold == MAX_CONNECTION) {
+                if (!(threads_number = (MAX_CONNECTION - MIN_THREAD_TRESHOLD) % (MIN_THREAD_TRESHOLD / 2)))
+                    threads_number = MIN_THREAD_TRESHOLD / 2;
             } else {
-                n_th = MIN_THREAD_TRESHOLD / 2;
+                threads_number = MIN_THREAD_TRESHOLD / 2;
             }
-            k -> min_active_threads_treshold -= n_th;
+            threads_sync_struct -> min_active_threads_treshold -= threads_number;
         }
     }
 
-    if (k -> threads_to_kill) {
-        n_th += k -> threads_to_kill;
-        k -> threads_to_kill = 0;
+    if (threads_sync_struct -> threads_to_kill) {
+        threads_number += threads_sync_struct -> threads_to_kill;
+        threads_sync_struct -> threads_to_kill = 0;
     }
 
-    for_kill(n_th, k);
+    mark_to_kill(threads_number, threads_sync_struct);
 }
 
